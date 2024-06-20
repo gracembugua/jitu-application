9496e2ce20e766c92cf0579b1e024b189425a0a9
gracembugua <114012742+gracembugua@users.noreply.github.com>
2024-06-20 11:09:43 +0300

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,13 +1,13 @@
-#Write a recursive function to calculate the factorial of a number in python
-def factorial(n):
-    if n == 0 or n == 1:
+#Write a recursive function to calculate the factorial of a number 
+def factorial(x):
+    if x == 0 or x == 1:
         return 1
     else:
-        return n * factorial(n - 1)
+        return x * factorial(x - 1)
 
 if __name__ == "__main__":
     num = int(input("Enter a number to calculate its factorial: "))
     if num < 0:
-        print("Factorial is not defined for negative numbers.")
+        print("negative number.")
     else:
         print(f"The factorial of {num} is: {factorial(num)}")
